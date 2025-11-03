03eb2b85a4df98aecf2855cd969a97236b9c52ff
Logan <logan@ljones.me>
2025-11-03 01:00:36 -0600

    wayyy more code generation: wip need google types

--- internal/codegen/codegen.go
+++ internal/codegen/codegen.go
@@ -6,10 +6,12 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"text/template"
 
-	"github.com/huandu/xstrings"
+	"github.com/Masterminds/sprig/v3"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/descriptorpb"
 )
 
@@ -35,11 +37,9 @@ type protoData struct {
 }
 
 type protoFile struct {
-	FileName             string
-	ProtoPath            string // original path of the proto file in the proto module that was injested
-	ProtoPathNoExtension string // original path of the proto file in the proto module that was injested
-	ClassName            string // camel case without .proto suffix
-	Messages             []protoMessage
+	ProtoPath string // original path of the proto file in the proto module that was injested
+	Messages  []protoMessage
+	Enums     []protoEnum
 }
 
 type protoMessage struct {
@@ -47,17 +47,24 @@ type protoMessage struct {
 	Fields      []protoMessageField
 }
 
+type protoEnum struct {
+	EnumName string
+	Options  []string
+}
+
 type protoMessageField struct {
-	FieldName string
-	GodotType string
+	FieldName    string
+	GodotType    string
+	IsCustomType bool
 }
 
 func NewCodeGenerator(logger *slog.Logger, destinationDirectoryPath, extensionName, protobufVersion string) (*CodeGenerator, error) {
-	tmpl, err := template.ParseFS(templatesFS, "templates/**/*.tmpl", "templates/**/**/*.tmpl")
+	tmpl, err := template.New("gdbuf").Funcs(sprig.FuncMap()).ParseFS(templatesFS, "templates/**/*.tmpl", "templates/**/**/*.tmpl")
 	if err != nil {
 		return nil, fmt.Errorf("could not parse code generation file templates: %w", err)
 	}
 
+	tmpl = tmpl.Funcs(sprig.FuncMap())
 	templates := tmpl.Templates()
 	for _, t := range templates {
 		logger.Debug("template loaded", "name", t.Name())
@@ -74,7 +81,7 @@ func NewCodeGenerator(logger *slog.Logger, destinationDirectoryPath, extensionNa
 
 func (cg *CodeGenerator) GenerateCode(fileDescriptorSet []*descriptorpb.FileDescriptorProto) error {
 	// first extract all of the data needed
-	protoData, err := extractProtoData(fileDescriptorSet)
+	protoData, err := cg.extractProtoData(fileDescriptorSet)
 	if err != nil {
 		return fmt.Errorf("problem extracting proto data: %w", err)
 	}
@@ -105,10 +112,10 @@ func (cg *CodeGenerator) GenerateCode(fileDescriptorSet []*descriptorpb.FileDesc
 
 	// generate for each proto file
 	for _, file := range templateData.ProtoData.Files {
-		cg.logger.Info("processing file", "name", file.FileName)
+		cg.logger.Info("processing file", "name", file.ProtoPath)
 		thisProtoFileTemplates := map[string]string{
-			"refcounted.h.tmpl":   fmt.Sprintf("src/%s.h", strings.TrimSuffix(file.FileName, ".proto")),
-			"refcounted.cpp.tmpl": fmt.Sprintf("src/%s.cpp", strings.TrimSuffix(file.FileName, ".proto")),
+			"refcounted.h.tmpl":   fmt.Sprintf("src/%s.h", strings.TrimSuffix(filepath.Base(file.ProtoPath), ".proto")),
+			"refcounted.cpp.tmpl": fmt.Sprintf("src/%s.cpp", strings.TrimSuffix(filepath.Base(file.ProtoPath), ".proto")),
 		}
 		for templateName, outputPath := range thisProtoFileTemplates {
 			err := os.MkdirAll(filepath.Dir(filepath.Join(cg.destinationDirectoryPath, outputPath)), 0755)
@@ -124,21 +131,104 @@ func (cg *CodeGenerator) GenerateCode(fileDescriptorSet []*descriptorpb.FileDesc
 	return nil
 }
 
-func extractProtoData(fileDescriptorSet []*descriptorpb.FileDescriptorProto) (*protoData, error) {
+func (cg *CodeGenerator) extractProtoData(fileDescriptorSet []*descriptorpb.FileDescriptorProto) (*protoData, error) {
 	var protoData protoData
+	// one loop through to get a mapping of filename and message name
+	var fileToMsgs map[string][]string = make(map[string][]string)
+	var fileToEnum map[string][]string = make(map[string][]string)
+	for _, file := range fileDescriptorSet {
+		for _, msg := range file.GetMessageType() {
+			cg.logger.Debug("found message", "file", file.GetName(), "message", msg.GetName())
+			fileToMsgs[file.GetName()] = append(fileToMsgs[file.GetName()], msg.GetName())
+		}
+		for _, enum := range file.GetEnumType() {
+			cg.logger.Debug("found enum", "file", file.GetName(), "enum", enum.GetName())
+			fileToEnum[file.GetName()] = append(fileToEnum[file.GetName()], enum.GetName())
+		}
+	}
+
+	// now for the real deal
 	for _, file := range fileDescriptorSet {
 		var protoFile protoFile
 		protoFile.ProtoPath = file.GetName()
-		protoFile.ProtoPathNoExtension = strings.TrimSuffix(protoFile.ProtoPath, ".proto")
-		protoFile.FileName = filepath.Base(protoFile.ProtoPathNoExtension)
-		protoFile.ClassName = xstrings.ToPascalCase(protoFile.FileName)
+
+		for _, enum := range file.GetEnumType() {
+			var protoEnum protoEnum
+			protoEnum.EnumName = enum.GetName()
+			enum.GetOptions().ProtoReflect().Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+				protoEnum.Options = append(protoEnum.Options, fd.TextName())
+				return true
+			})
+			protoFile.Enums = append(protoFile.Enums, protoEnum)
+		}
+
 		for _, msg := range file.GetMessageType() {
 			var protoMessage protoMessage
 			protoMessage.MessageName = msg.GetName()
+
 			for _, field := range msg.GetField() {
 				var protoMessageField protoMessageField
 				protoMessageField.FieldName = field.GetName()
-				godotType, err := mapProtoToGodotType(*field.GetType().Enum())
+				fieldType := *field.GetType().Enum()
+				fieldTypeName := field.GetTypeName()[strings.LastIndex(field.GetTypeName(), ".")+1:]
+				godotType := "UNKNOWN"
+				var err error
+				switch fieldType {
+				case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
+					var srcFile string = ""
+					switch fieldTypeName {
+					case "Any":
+						fallthrough
+					case "Timestamp":
+						fallthrough
+					case "Duration":
+						fallthrough
+					case "StringValue":
+						fallthrough
+					case "Int32Value":
+						fallthrough
+					case "Struct":
+						fallthrough
+					case "Empty":
+						srcFile = "google::protobuf"
+					default:
+						for f, _ := range fileToMsgs {
+							if slices.Contains(fileToMsgs[f], fieldTypeName) {
+								srcFile = f
+								break
+							}
+						}
+					}
+					if srcFile == "" {
+						return nil, fmt.Errorf("could not find source file for message type: %s", fieldTypeName)
+					}
+					protoMessageField.IsCustomType = true
+					if srcFile == protoFile.ProtoPath {
+						godotType = fieldTypeName
+					} else {
+						godotType = fmt.Sprintf("%s::%s", filepath.Base(strings.TrimSuffix(srcFile, ".proto")), fieldTypeName)
+					}
+				case descriptorpb.FieldDescriptorProto_TYPE_ENUM:
+					var srcFile string = ""
+					for f, _ := range fileToEnum {
+						if slices.Contains(fileToEnum[f], fieldTypeName) {
+							srcFile = f
+							break
+						}
+					}
+					if srcFile == "" {
+						return nil, fmt.Errorf("could not find source file for enum type: %s", fieldTypeName)
+					}
+					protoMessageField.IsCustomType = true
+					if srcFile == protoFile.ProtoPath {
+						godotType = fieldTypeName
+					} else {
+						godotType = fmt.Sprintf("%s::%s", filepath.Base(strings.TrimSuffix(srcFile, ".proto")), fieldTypeName)
+					}
+				default:
+					protoMessageField.IsCustomType = false
+					godotType, err = mapProtoToGodotType(fieldType)
+				}
 				if err != nil {
 					return nil, fmt.Errorf("could not map proto to godot type: %w", err)
 				}
--- internal/codegen/godot.go
+++ internal/codegen/godot.go
@@ -7,24 +7,23 @@ import (
 )
 
 var protoGodotTypeMap = map[descriptorpb.FieldDescriptorProto_Type]string{
-	descriptorpb.FieldDescriptorProto_TYPE_STRING: "Variant::STRING",
-	descriptorpb.FieldDescriptorProto_TYPE_BOOL:   "Variant::BOOL",
-	descriptorpb.FieldDescriptorProto_TYPE_INT32:  "Variant::INT",
-	descriptorpb.FieldDescriptorProto_TYPE_INT64:  "Variant::INT",
-	descriptorpb.FieldDescriptorProto_TYPE_FLOAT:  "Variant::FLOAT",
-	descriptorpb.FieldDescriptorProto_TYPE_BYTES:  "Variant::PACKED_BYTE_ARRAY",
-	descriptorpb.FieldDescriptorProto_TYPE_DOUBLE: "Variant::FLOAT",
-	// TODO: each of these need a mapping
-	descriptorpb.FieldDescriptorProto_TYPE_UINT32:   "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_UINT64:   "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_SINT32:   "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_SINT64:   "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_FIXED32:  "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_FIXED64:  "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_SFIXED32: "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_SFIXED64: "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:  "UNKNOWN",
-	descriptorpb.FieldDescriptorProto_TYPE_ENUM:     "UNKNOWN",
+	descriptorpb.FieldDescriptorProto_TYPE_STRING:   "godot::String",
+	descriptorpb.FieldDescriptorProto_TYPE_BOOL:     "bool",
+	descriptorpb.FieldDescriptorProto_TYPE_INT32:    "int32_t",
+	descriptorpb.FieldDescriptorProto_TYPE_INT64:    "int64_t",
+	descriptorpb.FieldDescriptorProto_TYPE_FLOAT:    "float",
+	descriptorpb.FieldDescriptorProto_TYPE_BYTES:    "godot::PackedByteArray",
+	descriptorpb.FieldDescriptorProto_TYPE_DOUBLE:   "double",
+	descriptorpb.FieldDescriptorProto_TYPE_UINT32:   "uint32_t",
+	descriptorpb.FieldDescriptorProto_TYPE_UINT64:   "uint64_t",
+	descriptorpb.FieldDescriptorProto_TYPE_SINT32:   "int32_t",
+	descriptorpb.FieldDescriptorProto_TYPE_SINT64:   "int64_t",
+	descriptorpb.FieldDescriptorProto_TYPE_FIXED32:  "int32_t",
+	descriptorpb.FieldDescriptorProto_TYPE_FIXED64:  "int64_t",
+	descriptorpb.FieldDescriptorProto_TYPE_SFIXED32: "int32_t",
+	descriptorpb.FieldDescriptorProto_TYPE_SFIXED64: "int64_t",
+	// descriptorpb.FieldDescriptorProto_TYPE_MESSAGE: "UNKNOWN",
+	// descriptorpb.FieldDescriptorProto_TYPE_ENUM:    "UNKNOWN",
 }
 
 func mapProtoToGodotType(protoType descriptorpb.FieldDescriptorProto_Type) (string, error) {
